910cdf925a28d074c4edbd37e5bdd31ed0e16798
foo <foo@example.com>
2017-11-27 17:38:39 +0530

    merge

--- config/environment.js
+++ config/environment.js
@@ -84,7 +84,7 @@ module.exports = function(environment) {
       // when it is created
       version: pkg.version,
       appName: 'nilavu',
-      apiServer: 'http://localhost:9636/',
+      apiServer: 'http://192.168.1.13:9636/',
       token: 'Bearer j83PF2_rzpace9btwu_WfvnObvQfqX2d5eRo3mynuXs',
       legacyApiEndpoint: '/v1',
       apiEndpoint: '/api/v1',
