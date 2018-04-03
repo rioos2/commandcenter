6bf30bd9155a3db01f1ab399ea712bccb912d13a
rathish <rathishvbr@outlook.com>
2018-04-03 15:47:16 +0530

    Console removed

--- app/authenticated/route.js
+++ app/authenticated/route.js
@@ -147,22 +147,18 @@ export default Ember.Route.extend(Subscribe, PromiseToCb, DefaultHeaders, {
     },
 
     switchProject(projectId, transition = true) {
-      console.log('Switch to ' + projectId);
       this.disconnectSubscribe(() => {
-        console.log('Switch is disconnected');
         this.send('finishSwitchProject', projectId, transition);
       });
     },
 
     finishSwitchProject(projectId, transition) {
-      console.log('Switch finishing');
       this.get('storeReset').reset();
       if (transition) {
         this.intermediateTransitionTo('authenticated');
       }
       this.set(`tab-session.${C.TABSESSION.PROJECT}`, projectId);
       this.refresh();
-      console.log('Switch finished');
     },
   },
 });
