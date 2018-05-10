57b2e7faa33d9f5ca146bf394cfbf7032ffe494b
suganyakaliyamoorthy <suganyakaliyamoorthy@megam.io>
2018-05-10 10:24:06 +0530

    add admin infra

--- app/admin/infra/controller.js
+++ app/admin/infra/controller.js
@@ -2,4 +2,12 @@ import Ember from "ember";
 const  {get} = Ember;
 import C from 'nilavu/utils/constants';
 export default Ember.Controller.extend({
+  selectedTab: 'node',
+  panels: [],
+
+    _initPanels: function() {
+        this.set('panels', []);
+        this.set('selectedTab', 'node');
+    }.on('init'),
+
 });
