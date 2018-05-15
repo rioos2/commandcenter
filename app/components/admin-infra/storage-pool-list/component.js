c10aebf362de6becf583971a84fff46b4ae96c82
suganya <suganyakaliyamoorthy@megam.io>
2018-05-15 06:07:35 +0000

    css changes for admin storage tab

--- app/components/admin-infra/storage-pool-list/component.js
+++ app/components/admin-infra/storage-pool-list/component.js
@@ -5,4 +5,16 @@ name: function(){
   return this.get('pool.object_meta.name');
 }.property('pool.object_meta.name'),
 
+status: function() {
+  return this.get('pool.status.phase');
+}.property('pool.status.phase'),
+
+diskCount: function(){
+  return this.get('pool.storage_info.disks.length');
+}.property('pool.storage_info.disks'),
+
+disks: function(){
+  return this.get('pool.storage_info.disks');
+}.property('pool.storage_info.disks'),
+
 });
