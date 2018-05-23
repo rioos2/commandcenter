9195ae9a2951e7418ede51084885468008dda276
suganyakaliyamoorthy <suganyakaliyamoorthy@megam.io>
2018-05-23 09:54:59 +0530

    network type based bridge list in virtual network add page

--- app/components/admin-infra/node-checkbox/component.js
+++ app/components/admin-infra/node-checkbox/component.js
@@ -9,24 +9,32 @@ export default Ember.Component.extend({
   intl: Ember.inject.service(),
 
   enabler: function() {
-    if (this.get('active')){
+    if (this.get('active')) {
       return "";
-  } else {
-    this.send('sendBridge', "");
-    return "disabled";
-  }
-}.property('active'),
+    } else {
+      this.send('sendBridge', "");
+      return "disabled";
+    }
+  }.property('active'),
 
-actions: {
-  sendType() {
-    this.toggleProperty('active');
-    this.sendAction('updatePoolData', this.get('active'), this.get('name'));
-  },
-  sendBridge(bridgeName) {
-    this.sendAction('setBridge', this.get('active'), {
-      name: this.get('data.node_id'),
-      value: bridgeName
-    });
-  },
-}
+  bridges: function() {
+    var self = this;
+    return this.get('data.bridges').map(function(bridge) {
+      if (bridge.types.includes(self.get('type')))
+        return bridge;
+    }).filter(val => val !== undefined);
+  }.property('type'),
+
+  actions: {
+    sendType() {
+      this.toggleProperty('active');
+      this.sendAction('updatePoolData', this.get('active'), this.get('name'));
+    },
+    sendBridge(bridgeName) {
+      this.sendAction('setBridge', this.get('active'), {
+        name: this.get('data.node_id'),
+        value: bridgeName
+      });
+    },
+  }
 });
