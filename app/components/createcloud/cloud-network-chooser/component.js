bd1a0113c7b8167fbfc7e0d6176db752bf21deb4
suganyakaliyamoorthy <suganyakaliyamoorthy@megam.io>
2018-05-29 12:44:26 +0530

    network filter by location

--- app/components/createcloud/cloud-network-chooser/component.js
+++ app/components/createcloud/cloud-network-chooser/component.js
@@ -12,41 +12,56 @@ export default Ember.Component.extend({
     "private_ipv6": "Private IPv6",
     "public_ipv6": "Public IPv6"
   },
+  disabledNetworks: [],
 
   initializeChart: Ember.on('didInsertElement', function() {
-    this.disableNetworks(this.networksFilter(this.get('model.networks.content')));
     this.waringMessage();
   }),
 
-  networksFilter: function(networks) {
-    var disableNetworks = [];
-    for (var k in this.get('networks')) disableNetworks.push(k);
+  selectedVirtualNetworks: function() {
+    var location;
+    var virtualNetworks = [];
+    var self = this;
+    Object.keys(this.get('networks')).map(function(key) {
+      virtualNetworks.addObject(key);
+    });
 
-    var filtered = [];
-    networks.forEach(function(network) {
-      filtered.pushObject(network.network_type);
-    }.bind(this));
-    filtered = [...new Set(filtered)];
+    this.get('disabledNetworks').map(function(disable) {
+      self.set(disable, "");
+    });
 
-    disableNetworks = disableNetworks.filter(function(el) {
-      return filtered.indexOf(el) < 0;
-    }.bind(this));
-    return disableNetworks;
-  },
+    if (!Ember.isEmpty(this.get('model.assemblyfactory.object_meta.cluster_name')) && !Ember.isEmpty(this.get('model.datacenters.content'))) {
+      location = this.get('model.datacenters.content').filter(function(location) {
+        if (Ember.isEqual(location.object_meta.name, self.get('model.assemblyfactory.object_meta.cluster_name'))) {
+          return location;
+        }
+      });
+      if (!Ember.isEmpty(location) && !Ember.isEmpty(self.get('model.networks.content'))) {
+        location[0].networks.map(function(network_id) {
+          self.get('model.networks.content').map(function(network) {
+            if (Ember.isEqual(network.id, network_id)) {
+              virtualNetworks.removeObject(network.network_type);
+            }
+          });
+        });
+      }
+    }
+    this.disableNetworks(virtualNetworks);
+  }.property('model.assemblyfactory.object_meta.cluster_name'),
 
-  disableNetworks: function(disableNetworks=[]) {
+  disableNetworks: function(disableNetworks = []) {
     this.set('disabledNetworks', disableNetworks);
     disableNetworks.forEach(function(disable) {
       this.set(disable, "disable-network");
     }.bind(this));
   },
 
-  waringMessage: function(){
-    if(Ember.isEmpty(this.get('model.networks.content'))){
+  waringMessage: function() {
+    if (Ember.isEmpty(this.get('model.networks.content'))) {
       this.get('notifications').warning(get(this, 'intl').t('notifications.network.empty'), {
         autoClear: true,
         clearDuration: 6000,
-        cssClasses:'notification-warning'
+        cssClasses: 'notification-warning'
       });
     }
   },
@@ -63,20 +78,27 @@ export default Ember.Component.extend({
 
   actions: {
     selected: function(net_type) {
+      var self=this;
+      this.set('model.assemblyfactory.network', "");
       this.toggleProperty(net_type + '_check');
       if (this.checkActiveNetwork(net_type)) {
         var cc = this.get("model.assemblyfactory.resources");
         if (!cc[net_type]) {
           cc[net_type] = "true";
           this.set(net_type, "selected");
-          this.set('model.assemblyfactory.network', net_type);
         } else {
           this.set(net_type, "");
           delete cc[net_type];
-          this.set('model.assemblyfactory.network', "");
         }
         this.set("model.assemblyfactory.resources", cc);
       }
+      Object.keys(this.get('networks')).map(function(key) {
+        Object.keys(self.get('model.assemblyfactory.resources')).map(function(network_type) {
+          if(Ember.isEqual(key,network_type)){
+            self.set('model.assemblyfactory.network', network_type);
+          }
+        });
+      });
     }
   }
 
