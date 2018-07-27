eb8ee2f2181b12b71aab3ef46f56c0d06c505433
rathish <rathishvbr@outlook.com>
2018-07-27 18:58:41 +0530

    Updated dashboard and model rsvp implementation

--- app/utils-old/denormalize.js
+++
@@ -1,6 +0,0 @@
-import C from 'nilavu/utils/constants';
-function denormalizeName(str) {
-  return str.replace(new RegExp('['+C.SETTING.DOT_CHAR+']','g'),'.').toLowerCase();
-}
-
-export {denormalizeName };
