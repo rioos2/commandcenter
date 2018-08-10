db0324a86b447d5940839367117598281c1c41d7
suganyakaliyamoorthy <suganyakaliyamoorthy@megam.io>
2018-08-10 15:00:06 +0530

    eslint error clear for  authenticated

--- app/authenticated/project/route.js
+++ app/authenticated/project/route.js
@@ -1,8 +1,7 @@
-import Ember from 'ember';
-import C from 'nilavu/utils/constants';
-
-export default Ember.Route.extend({
-  access: Ember.inject.service(),
+import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+export default Route.extend({
+  access: service(),
 
   loadingError(err, transition, ret) {
     if (err && err.status && [401, 403].indexOf(err.status) >= 0) {
