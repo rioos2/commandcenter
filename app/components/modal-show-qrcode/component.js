bd2b2801d521d9bbc08f004c6c1460661afafbe0
rathish <rathishvbr@outlook.com>
2018-09-07 13:44:27 +0530

    QR code note and heading changed

--- app/components/modal-show-qrcode/component.js
+++ app/components/modal-show-qrcode/component.js
@@ -1,13 +1,22 @@
 import ModalBase from 'lacsso/components/modal-base';
 import { alias } from '@ember/object/computed';
+import { htmlSafe } from '@ember/string';
+import { inject as service } from '@ember/service';
+import { computed } from '@ember/object';
+import { get } from '@ember/object';
+
+
 
 export default ModalBase.extend({
+  intl:          service(),
+
+
   classNames:    ['lacsso', 'modal-container', 'large-modal', 'modal-overlay-width'],
   originalModel: alias('modalService.modalOpts'),
 
-  name: function() {
-    return this.get('originalModel.key');
-  }.property('originalModel.key'),
+  note: computed('originalModel.key', function() {
+    return htmlSafe(get(this, 'intl').t('QRCode.qrNote'));
+  }),
 
   actions: {
     cancel() {
