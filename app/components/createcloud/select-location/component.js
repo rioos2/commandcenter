31dc6dc2cb8b9bc85541c8442da96c0d34547ea0
rajthilak <rajthilak@megam.io>
2018-09-29 21:14:49 +0530

    fix globe and locations search

--- app/components/createcloud/select-location/component.js
+++ app/components/createcloud/select-location/component.js
@@ -5,22 +5,25 @@ import { on } from '@ember/object/evented';
 import { inject as service } from '@ember/service';
 
 export default Component.extend({
-  notifications:   service('notification-messages'),
-  showField:       false,
+
+  notifications: service('notification-messages'),
+  showField:     false,
+
   initializeChart: on('didInsertElement', function() {
     this.set('model.locationList', this.getCountry(this.get('model')));
-
     renderGlobeChart(this.get('model'), this.get('notifications')); // eslint-disable-line
   }),
 
-  locationAvailable: function(){
+  locationAvailable: function() {
     return this.get('model.datacenters.content').length > 0;
   }.property('model.datacenters.content'),
+
 
   actions: {
     showInputField() {
       this.set('showField', true);
     },
+
     closeInputField() {
       this.set('showField', false);
     },
@@ -29,39 +32,37 @@ export default Component.extend({
       renderGlobeChart.getLocation(); // eslint-disable-line
     }
   },
+
   getCountry(model) {
     const self = this;
     let features = model.datacenters.content.map((x) => {
-      alert(JSON.stringify(x));
-
       return {
         'type':     'Feature',
         'City':     x.object_meta.name,
         'geometry': {
           'type':        'Point',
-          'coordinates': self.getCoordinates(x.object_meta.name)
+          'coordinates': self.getCoordinates(x.object_meta.name, x.advanced_settings.country_code)
         }
       }
     });
+
     let country = {
-      'type':     'FeatureCollection',
+      'type': 'FeatureCollection',
       features,
     };
 
     return country;
   },
 
-  getCoordinates(q) {
-    let m = new RioGeo().fillWithGeoInfo(q, 'US');
-    alert(JSON.stringify(m));
-    const lkv = R.pick(['lat', 'lng'], m);
 
-    alert(JSON.stringify(lkv));
-    const latlng = R.values(lkv);
+  getCoordinates(q, r) {
 
-    alert(JSON.stringify(latlng));
+    let m = new RioGeo().fillWithGeoInfo(q, r);
 
+    const lm = (l) => R.props(['lng', 'lat'], l);
+    const lnglat = R.map(lm)(m);
 
+    return  R.flatten(lnglat);
   },
 
 });
