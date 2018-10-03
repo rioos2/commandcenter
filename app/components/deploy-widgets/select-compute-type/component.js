745b9caa6c81e59f985336e7a77c631afcd4c28a
Your Name <vigneshvilha@megam.io>
2018-10-03 15:45:28 +0530

    creating digital cloud and creating container changes in deploy fixed

--- app/components/deploy-widgets/select-compute-type/component.js
+++ app/components/deploy-widgets/select-compute-type/component.js
@@ -1,20 +1,29 @@
 import Component from '@ember/component';
 import C from 'nilavu/utils/constants';
-import { alias } from '@ember/object/computed';
-import {
-  get, set, computed
-} from '@ember/object';
+import {  alias} from '@ember/object/computed';
+import {  get,set,  computed } from '@ember/object';
+import { isEmpty } from '@ember/utils';
 
 export default Component.extend({
 
   showDescription: false,
+  labels: alias('stacksfactory.object_meta.labels'),
+  compute: alias('stacksfactory.resources.compute_type'),
 
-  compute:       alias('stacksfactory.resources.compute_type'),
+
 
   isSelectedCPU: computed('compute', function() {
     return get(this, 'compute') === C.VPS.RESOURSE_COMPUTE_TYPE.CPU;
   }),
 
+  title: computed('labels', function() {
+    let c = get(this, 'labels.rioos_category');
+    if (isEmpty(c)) {
+      c = C.CATEGORIES.MACHINE;
+    }
+    const ci = `launcherPage.${ c }.title`;
+    return ci;
+  }),
 
   actions: {
 
@@ -39,5 +48,6 @@ export default Component.extend({
     closeDescription() {
       set(this, 'showInfo', false);
     }
+
   }
 });
